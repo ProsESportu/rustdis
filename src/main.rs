4c7a18ea9e0c102aa62396aba2cd12592f937f05
ProsESportu <t.hajduga@poczta.fm>
2023-05-22 22:01:03 +0200

    hjh

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
+use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
+use crypto::scrypt::{scrypt_check, scrypt_simple, ScryptParams};
+use serde::{Deserialize, Serialize};
 use std::{
     collections::HashMap,
     fs::File,
     io::{Result, Write},
     sync::Mutex,
 };
-
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use serde::{Deserialize, Serialize};
 #[derive(Serialize)]
 struct GetPost {
     result: String,
     status: String,
 }
-#[derive(Deserialize)]
-#[derive(Debug)]
+#[derive(Deserialize, Debug)]
 struct GetInput {
     id: String,
     password: String,
@@ -23,13 +22,15 @@ struct GetInput {
 }
 
 #[derive(Deserialize)]
-struct SetPostInput {
+struct SetInput {
     id: String,
     value: String,
     save: Option<bool>,
+    user: String,
+    password: String,
 }
 #[derive(Serialize)]
-struct SetPostOutput {
+struct SetOutput {
     id: String,
     value: String,
     status: String,
@@ -48,8 +49,9 @@ async fn main() -> Result<()> {
             print!("enter the root password:");
             std::io::stdout().flush().unwrap();
             std::io::stdin().read_line(&mut pass).unwrap();
+            let hash = scrypt_simple(&pass, &ScryptParams::new(15, 8, 1)).unwrap();
             let mut json = HashMap::new();
-            json.insert("root".to_string(), pass);
+            json.insert("root".to_string(), hash);
             let file = File::create("./users.json").unwrap();
             serde_json::to_writer(file, &json).unwrap();
             json
@@ -69,7 +71,6 @@ async fn main() -> Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(state.clone())
-            .service(get_get)
             .service(set_post)
             .service(get_post)
     })
@@ -81,58 +82,54 @@ fn save(data: &HashMap<String, String>) {
     let file = File::create("./db.json").unwrap();
     serde_json::to_writer(file, data).unwrap();
 }
-#[get("/")]
-async fn get_get(state: web::Data<AppState>, query: web::Query<GetInput>) -> impl Responder {
-    return match state
-        .hash_map
-        .lock()
-        .expect("something went wrong")
-        .get(&query.id)
-    {
-        Some(res) => HttpResponse::Ok().body(res.to_string()),
-        None => HttpResponse::NotFound().body("Not found"),
-    };
-}
-
 #[post("/get")]
 async fn get_post(req: web::Json<GetInput>, state: web::Data<AppState>) -> impl Responder {
-
     match state.users.get(&req.user) {
-        Some(e) => match *e == req.password {
-            true => match state
-                .hash_map
-                .lock()
-                .expect("something went wrong")
-                .get(&req.id)
-            {
-                Some(dt) => HttpResponse::Ok().json(GetPost {
-                    result: dt.to_string(),
-                    status: "ok".to_string(),
-                }),
-                None => HttpResponse::NotFound().json(GetPost {
-                    result: "".to_string(),
-                    status: "not_found".to_string(),
-                }),
-            },
-            false => HttpResponse::Forbidden().body("acees deneid"),
+        Some(e) => match scrypt_check(&req.password, e) {
+            Ok(_) => {
+                match state
+                    .hash_map
+                    .lock()
+                    .expect("something went wrong")
+                    .get(&req.id)
+                {
+                    Some(dt) => HttpResponse::Ok().json(GetPost {
+                        result: dt.to_string(),
+                        status: "ok".to_string(),
+                    }),
+                    None => HttpResponse::NotFound().json(GetPost {
+                        result: "".to_string(),
+                        status: "not_found".to_string(),
+                    }),
+                }
+            }
+            Err(_) => HttpResponse::Forbidden().body("wrong password"),
         },
         None => HttpResponse::Forbidden().body("no such user"),
     }
 }
 
-#[post("/")]
-async fn set_post(state: web::Data<AppState>, req: web::Json<SetPostInput>) -> impl Responder {
-    let mut map = state.hash_map.lock().expect("something went wrong");
-    map.insert(req.id.to_string(), req.value.to_string());
+#[post("/set")]
+async fn set_post(state: web::Data<AppState>, req: web::Json<SetInput>) -> impl Responder {
+    match state.users.get(&req.user) {
+        Some(e) => match scrypt_check(&req.password, &e) {
+            Ok(_) => {
+                let mut map = state.hash_map.lock().expect("something went wrong");
+                map.insert(req.id.to_string(), req.value.to_string());
 
-    match req.save {
-        Some(true) => save(&state.hash_map.lock().unwrap()),
-        _ => println!("not saved"),
+                match req.save {
+                    Some(true) => save(&state.hash_map.lock().unwrap()),
+                    _ => println!("not saved"),
+                }
+
+                HttpResponse::Ok().json(SetOutput {
+                    id: req.id.to_string(),
+                    value: req.value.to_string(),
+                    status: "ok".to_string(),
+                })
+            }
+            Err(_)=>HttpResponse::Forbidden().body("wrong password")
+        },
+        None => HttpResponse::Forbidden().body("no such user"),
     }
-
-    HttpResponse::Ok().json(SetPostOutput {
-        id: req.id.to_string(),
-        value: req.value.to_string(),
-        status: "ok".to_string(),
-    })
 }
